1aff7dcf951b89a9cedd128411e130cdca8542b9
Davrann <davran09@gmail.com>
2019-10-16 22:03:19 +0600

    111

--- printcomb.go
+++ printcomb.go
@@ -3,7 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	next := false 
+	next := false
 	for a := '0'; a <= '9'; a++ {
 		for b := a + 1; b <= "9"; b++ {
 			for c := b + 1; c <= '9'; c++ {
