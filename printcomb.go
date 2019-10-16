f58c9520328bdb7d15470fa1a17bbdf0ee430dfe
Davrann <davran09@gmail.com>
2019-10-16 22:04:21 +0600

    1121

--- printcomb.go
+++ printcomb.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	next := false
 	for a := '0'; a <= '9'; a++ {
-		for b := a + 1; b <= "9"; b++ {
+		for b := a + 1; b <= '9'; b++ {
 			for c := b + 1; c <= '9'; c++ {
 				if next {
 					z01.PrintRune(',')
