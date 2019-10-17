324da5432f34408498cd29cc59bbdbf961e9983f
Davran <53410133+Davrann@users.noreply.github.com>
2019-10-17 17:33:44 +0600

    Delete myfamily.sh

--- myfamily.sh
+++
@@ -1,1 +0,0 @@
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson HeroID "$HERO_ID" '.[] | select( .id ==$HeroID) | .connections.relatives' | sed 's/"//g'
